23c52173112df1e7ec055df2b5a0b69bc04eaf1e
Renette Ros <renetteros@gmail.com>
2015-07-01 17:48:19 +0200

    Unity Game

---
+++ Unity Game/Assets/scripts/FauxGravityBody.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FauxGravityBody : MonoBehaviour {
+
+	public FauxGravityAttractor attractor;
+	private Transform myTransform;
+	// Use this for initialization
+	void Start () {
+		var rigidbody = GetComponent<Rigidbody> ();
+		rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+		rigidbody.useGravity = false;
+		myTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		attractor.attract(myTransform);
+	}
+}
---
+++ Unity Game/Assets/scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed = 15;
+	public Vector3 moveDir;
+	
+	// Update is called once per frame
+	void Update () {
+		moveDir = new Vector3 (Input.GetAxisRaw("Horizontal"), 0 , Input.GetAxisRaw("Vertical")).normalized;
+	}
+
+	void FixedUpdate() {
+		var rigidbody = GetComponent<Rigidbody> ();
+		rigidbody.MovePosition (rigidbody.position + transform.TransformDirection(moveDir) * moveSpeed * Time.deltaTime);
+	}
+}
