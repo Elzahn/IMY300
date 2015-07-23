b009878853adb3af0a7cdfe2f0b991d7215a9e92
Elzahn Botha <elzahnbotha@mweb.co.za>
2015-07-23 22:18:15 +0200

    Trees Spawning

--- Unity Game/Assets/scripts/TestPositioning.cs
+++ Unity Game/Assets/scripts/TestPositioning.cs
@@ -7,7 +7,7 @@ public class TestPositioning : MonoBehaviour {
 		
 		public GameObject tree1;
 		
-		const int TREE_COUNT = 10;
+		const int TREE_COUNT = 100;
 		
 		FauxGravityAttractor planet;
 		
@@ -23,38 +23,64 @@ public class TestPositioning : MonoBehaviour {
 				addTree(tree);
 			}	  
 		}
-		
+
 		GameObject chooseTree(int i) {
 			switch(i) {
 			default : 
 				return tree1;
 			}
 		}
-		
-		void addTree(GameObject tree) {	
-			
-			GameObject go = Instantiate(tree);
-			go.AddComponent<FauxGravityBody>();
-			go.AddComponent<Rigidbody> ();
-		go.transform.localScale = new Vector3 (0.0025f, 0.0025f, 0.0025f);
 
-			GameObject child = go.transform.FindChild ("Cylinder001").gameObject;
+	void position(GameObject go){
+		int tries = 6;
+		Mesh mesh = GameObject.Find ("Planet").GetComponent<MeshFilter> ().mesh;
+		Vector3 position;
 
-			child.tag = "WorldObject";
-			child.AddComponent<MeshCollider> ();
-			child.GetComponent<MeshCollider> ().convex = true;
-			
-			
-			go.GetComponent<FauxGravityBody>().attractor = planet;
-			go.tag = "WorldObject";
-			
-			//TODO Position correctly
-			Mesh mesh = GameObject.Find ("Planet").GetComponent<MeshFilter> ().mesh;
-			//go.transform.position = Random.insideUnitSphere * ((mesh.bounds.size.y/4));//bounds.size.y/8); 
-			
-			go.transform.GetComponent<Rigidbody> ().position = Random.onUnitSphere * ((mesh.bounds.size.y/10));//bounds.size.y/8); 
-			
-			trees.AddLast(go);
-			
+		while (tries > 0) {
+			tries--;
+			position = Random.onUnitSphere * ((GameObject.Find("Planet").transform.lossyScale.x/2));
+			print(position + " " + mesh.bounds.size.y);
+			Collider[] collidedItems = Physics.OverlapSphere(position, 0.05f);
+			List<Collider> tempList = new List<Collider>();
+
+			foreach(Collider col in collidedItems){
+				if(col.name != "Planet" && col.transform != go.transform){
+					tempList.Add(col);
+				}
+			}
+
+			if(tempList.Count() == 0){
+				go.transform.parent.gameObject.transform.GetComponent<Rigidbody> ().position = position;
+				GameObject.Find(go.transform.parent.gameObject.name).GetComponent<myTree>().check = Time.time;
+
+				//GameObject.Find(go.transform.parent.gameObject.name).GetComponent<myTree>().pos = position;
+				return;
+			}
 		}
+
+		Destroy(go.transform.parent.transform.gameObject);
+	}
+
+	void addTree(GameObject tree) {	
+			
+		GameObject go = Instantiate(tree);
+		/*go.AddComponent<FauxGravityBody>();
+		go.AddComponent<Rigidbody> ();*/
+		go.transform.localScale = new Vector3 (0.025f, 0.025f, 0.025f);	//size influences attraction to planet too small floates
+		//go.AddComponent<myTree> ();
+
+		GameObject child = go.transform.FindChild ("Cylinder001").gameObject;
+
+		/*child.tag = "WorldObject";
+		child.AddComponent<MeshCollider> ();
+		child.GetComponent<MeshCollider> ().convex = true;
+			*/
+		go.GetComponent<FauxGravityBody>().attractor = planet;
+		//go.tag = "WorldObject";
+		/*Mesh mesh = GameObject.Find ("Planet").GetComponent<MeshFilter> ().mesh;
+		go.transform.GetComponent<Rigidbody> ().position = Random.onUnitSphere * Random.value * ((mesh.bounds.size.y));
+		*/
+		position (child);
+		trees.AddLast(go);
+	}
 }
---
+++ Unity Game/Assets/scripts/myTree.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class myTree : MonoBehaviour {
+
+	/*private bool posSet = false;
+	private Vector3 pos;*/
+	public float check;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+
+	//check collision and set freezAll / isKinematic
+
+	void OnCollisionEnter (Collision col){
+		if (col.collider.name == "Planet") {
+		//	this.GetComponent<Rigidbody>().isKinematic = true;//constraints = RigidbodyConstraints.FreezeAll;
+		}
+	}
+
+	//check in update after time if not iskinematic;
+	//change unfreez to false of kinematic
+
+	/*public void setTree( ){
+		pos = this.GetComponent<Rigidbody> ().position;
+		//this.GetComponent<Rigidbody> ().position = position;
+		posSet = true;
+		check = Time.time;
+	}*/
+
+	// Update is called once per frame
+	void Update () {
+
+		if(Time.time >= check + 1f && this.GetComponent<Rigidbody> ().constraints != RigidbodyConstraints.FreezeAll) {
+			print ("destroy");
+			Destroy(this.gameObject);
+		}
+	}
+}
