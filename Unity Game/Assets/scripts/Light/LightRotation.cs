849c6b584d7674d4da0e18a18672dc22eb7cd09b
Renette Ros <renetteros@gmail.com>
2015-07-29 13:59:45 +0200

    Added missing files

---
+++ Unity Game/Assets/scripts/Light/LightRotation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightRotation : MonoBehaviour {
+
+	public int lightSpeed = 20;
+
+	
+	// Update is called once per frame
+	void Update () {
+		PlayerController playerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+		if (!playerScript.paused) {
+			transform.RotateAround(Vector3.zero, Vector3.up, lightSpeed * Time.deltaTime);
+		}
+	}
+
+	public const int DUSK_RANGE = 10;
+	public static string getDark(GameObject other) {
+		GameObject sun = GameObject.Find("Sun");
+		Vector3 direction = (sun.transform.position - other.transform.position).normalized;
+		float angle = Vector3.Angle(other.transform.up, direction);
+		if (angle <= 90-DUSK_RANGE) {
+			return "light";
+		} else if (angle < 90 + DUSK_RANGE) {
+			return "dusk";
+		}
+		return "dark";
+
+	}
+}
