c6a7d40e7a29a0bb4dd58014affb6939c1d0e60b
Elzahn Botha <elzahnbotha@mweb.co.za>
2015-09-01 19:34:32 +0200

    HUD slide and scroll

--- Unity Game/Assets/scripts/Menu/HUD.cs
+++ Unity Game/Assets/scripts/Menu/HUD.cs
@@ -4,23 +4,46 @@ using System.Collections;
 
 public class HUD : MonoBehaviour {
 	private bool expandTheHud;
+	private bool shrinkTheHud;
+
+	private Image expandingHUD;
+	private Image shrinkButton;
+	private Image expandButton;
+	//private Image hudText
+
+	void Start(){
+		expandingHUD = GameObject.Find ("Expand").GetComponent<Image> ();
+		shrinkButton = GameObject.Find ("Shrink_Button").GetComponent<Image> ();
+		expandButton = GameObject.Find ("Expand_Button").GetComponent<Image> ();
+		shrinkButton.enabled = true;
+		expandButton.enabled = false;
+		expandingHUD.fillAmount = 1;
+	}
 
 	public void expandHud(){
 		expandTheHud = true;
+		shrinkTheHud = false;
+		expandButton.enabled = false;
+		shrinkButton.enabled = true;
+	}
+
+	public void shrinkHUD(){
+		shrinkTheHud = true;
+		expandTheHud = false;
+		expandButton.enabled = true;
+		shrinkButton.enabled = false;
 	}
 
 	void Update(){
-		//float expandHUDpanel = Time.time;
 		if (expandTheHud) {
-			Image expandingHUD = GameObject.Find ("Expand").GetComponent<Image> ();
-		
 			if (expandingHUD.fillAmount < 1) {
-				//if(Time.time >= expandHUDpanel + 1)
-				{
-					//	print (expandingHUD.fillAmount);
-					expandingHUD.fillAmount += 0.05f;
-					//expandHUDpanel = Time.time;
-				}
+				expandingHUD.fillAmount += 0.05f;
+			}
+		}
+
+		if (shrinkTheHud) {
+			if(expandingHUD.fillAmount > 0){
+				expandingHUD.fillAmount -= 0.05f;
 			}
 		}
 	}
