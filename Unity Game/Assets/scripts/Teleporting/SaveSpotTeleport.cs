73585360145ca610d43659f4dd3640dcb90d3ec6
Elzahn Botha <elzahnbotha@mweb.co.za>
2015-09-29 21:15:12 +0200

    Started warpPoint placement
    Fixed character rotation between scenes

--- Unity Game/Assets/scripts/Teleporting/SaveSpotTeleport.cs
+++ Unity Game/Assets/scripts/Teleporting/SaveSpotTeleport.cs
@@ -14,7 +14,7 @@ public class SaveSpotTeleport : MonoBehaviour {
 	public bool notInUse {get; set;}
 	public bool showedHealthHint {get; set;}
 
-	private bool showExitConfirmation, showEntranceConfirmation, showNoEntry;
+	private bool showExitConfirmation, showEntranceConfirmation, showNoEntry, justWarped;
 	private PlayerAttributes attributesScript;
 	private Sounds sound;
 
@@ -36,6 +36,7 @@ public class SaveSpotTeleport : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		showedHealthHint = false;
+		justWarped = false;
 		notInUse = true;
 		Hud = Camera.main.GetComponent<HUD> ();
 		hint = GameObject.Find ("Hint").GetComponent<Image> ();
@@ -77,22 +78,20 @@ public class SaveSpotTeleport : MonoBehaviour {
 			GameObject.Find ("Bedroom Light").GetComponent<Light> ().enabled = false;
 		}
 
+		//Position player when warping into the ship
+		if(Application.loadedLevelName == "SaveSpot" && this.GetComponent<Tutorial>().tutorialDone && !justWarped){
+			justWarped = true;
+			this.transform.rotation = Quaternion.Euler(351.66f, 179.447f, 358.8f);
+			this.transform.up = Vector3.up;
+			this.transform.position = new Vector3 (12.99f, 81.45f, 15.25f);
+		}
+
 		if (showExitConfirmation && loadTutorial) {
 			notInUse = false;
 			Hud.makeInteractionHint ("Press E to start the tutorial", pressE);
-			//interaction_Button.sprite = pressE;
-			//interaction_Button.GetComponent<Mask> ().enabled = false;
-			/*if(!textAdded){
-				expandText.text += "\nPress E to start the tutorial";
-				Canvas.ForceUpdateCanvases();
-				scrollbar.value = 0f;
-				textAdded = true;
-			}*/
 		} else if (showNoEntry && !loadTutorial && !showedHealthHint) {
 			notInUse = false;
 			Hud.makeInteractionHint ("Kill the boss and take his loot to go back to the ship.", noEntry);
-			/*interaction_Button.sprite = noEntry;
-			interaction_Button.GetComponent<Mask> ().enabled = false;*/
 		} else if (showEntranceConfirmation) {
 			notInUse = false;
 			Hud.makeInteractionHint ("Press E to teleport. Remember coming back starts the next level.", pressE);
@@ -104,7 +103,6 @@ public class SaveSpotTeleport : MonoBehaviour {
 		}
 
 		if (Application.loadedLevelName == "Tutorial" && showEntranceConfirmation && Input.GetKeyDown (KeyCode.E) && Loot.gotPowerCore == true) {
-			//showExitConfirmation = true;
 			canEnterSaveSpot = false;
 			GameObject.Find("Player").GetComponent<PlayerAttributes>().storage.AddLast(new Cupcake());
 			showEntranceConfirmation = false;
@@ -112,13 +110,9 @@ public class SaveSpotTeleport : MonoBehaviour {
 			attributesScript.restoreHealthToFull();
 			attributesScript.restoreStaminaToFull();
 			this.GetComponent<Rigidbody>().mass = 100;
-			this.transform.rotation = new Quaternion(0, 0.7f, 0, 0.7f);
-			//this.transform.position = new Vector3 (-27.01f, 79.65f, 1.93f);
-			this.transform.position = new Vector3 (13.72f, 81.58f, 14.77f);//(9.4f, 81.38f, 6.62f);
 
 			this.GetComponent<LevelSelect>().currentLevel++;
 			interaction.fillAmount = 0;
-			//this.transform.up = GameObject.Find("Floor").transform.up;
 			Application.LoadLevel ("SaveSpot");
 			Resources.UnloadUnusedAssets();
 		} else if (showExitConfirmation && Input.GetKeyDown (KeyCode.E) && !loadTutorial) {
@@ -127,11 +121,10 @@ public class SaveSpotTeleport : MonoBehaviour {
 			this.GetComponent<Rigidbody> ().mass = 0.1f;
 			sound.playWorldSound (Sounds.TELEPORTING);
 			attributesScript.saveInventoryAndStorage ();
-			//GameObject.Find("Player").transform.position = new Vector3(9.41f, 79.19f, 7.75f);
 			this.GetComponent<Tutorial>().stopTutorial();
 			interaction.fillAmount = 0;
 			this.GetComponent<Tutorial> ().startTutorial = false;
-
+			justWarped = false;
 			Application.LoadLevel ("Scene");
 			Resources.UnloadUnusedAssets();
 		} else if(showExitConfirmation && Input.GetKeyDown(KeyCode.E) && loadTutorial){
@@ -140,27 +133,23 @@ public class SaveSpotTeleport : MonoBehaviour {
 			this.GetComponent<Rigidbody> ().mass = 0.1f;
 			sound.playWorldSound (Sounds.TELEPORTING);
 			attributesScript.saveInventoryAndStorage ();
-			//this.transform.position = new Vector3(0f, 15.03f, 0);
-			//this.transform.position = new Vector3(-0.01f, 16.149f, -0.27f);
 			interaction.fillAmount = 0;
 			sound.stopSound("computer");
 			this.GetComponent<LevelSelect>().currentLevel = 0;
 			interaction.fillAmount = 0;
+			justWarped = false;
 			Application.LoadLevel("Tutorial");
 			showExitConfirmation = false;
 			//Set it once Run hint pasts	loadTutorial = false;
 			this.transform.position = new Vector3(0, 16f, -0.327f);
 			Resources.UnloadUnusedAssets();
 		}else if (showEntranceConfirmation && Input.GetKeyDown (KeyCode.E) && Application.loadedLevelName != "Tutorial") {
-			//showExitConfirmation = true;
-
 			showEntranceConfirmation = false;
 			attributesScript.restoreHealthToFull();
 			attributesScript.restoreStaminaToFull();
 			this.GetComponent<Rigidbody>().mass = 100;
 			this.transform.rotation = new Quaternion(0, 0.7f, 0, -0.7f);
-			//this.transform.position = new Vector3 (-27.01f, 79.65f, 1.93f);
-			this.transform.position = new Vector3 (13.72f, 81.58f, 14.77f);//(9.4f, 81.38f, 6.62f);
+			this.transform.position = new Vector3 (13.72f, 81.58f, 14.77f);
 
 			this.GetComponent<LevelSelect>().currentLevel++;
 			this.GetComponent<LevelSelect>().spawnedLevel = false;
@@ -173,15 +162,7 @@ public class SaveSpotTeleport : MonoBehaviour {
 		}
 	}
 
-	/*void OnGUI(){
-		if (showNoEntry) {
-			//GUI.Box (new Rect (140, Screen.height - 50, Screen.width - 300, 120), ("Kill the boss and take his loot to go back."));
-		} else if (showExitConfirmation && loadTutorial) {
-			//GUI.Box (new Rect (140, Screen.height - 50, Screen.width - 300, 120), ("Press E to start the tutorial"));
-		} else if (showExitConfirmation){
-			//GUI.Box (new Rect (140, Screen.height - 50, Screen.width - 300, 120), ("Press E to go outside. Remember you can only come back once the level has been cleared."));
-		} else if (showEntranceConfirmation) {
-			//GUI.Box(new Rect(140,Screen.height-50,Screen.width-300,120),("Press E to enter. Remember once you have entered coming back starts the next level."));
-		}
-	}*/
+	void setCharacterRotation(Vector3 location, Quaternion rotation){
+
+	}
 }
