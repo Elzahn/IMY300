5daa84fdca03a664ad7051d67a5366b8330336e7
Elzahn Botha <elzahnbotha@mweb.co.za>
2015-10-08 12:57:47 +0200

    Start of main menu

--- Unity Game/Assets/scripts/UI/MenuScript.cs
+++ Unity Game/Assets/scripts/UI/MenuScript.cs
@@ -1,9 +1,49 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class MenuScript : MonoBehaviour {
 
- public void OnClickPlay(){
-  Application.LoadLevel("SaveSpot");
- }
+	public Font alienFont;
+	public Font defaultFont;
+
+	private PlayerAttributes attributesScript;
+
+	void Start(){
+		attributesScript = GameObject.Find ("Player").GetComponent<PlayerAttributes> ();
+		GameObject.Find ("Player").GetComponent<Tutorial> ().enabled = false;
+	}
+
+	public void changeFont(){
+		Text textMesh = this.GetComponentInChildren<Text>();
+
+		if (textMesh.font == defaultFont) {
+			textMesh.font = alienFont;
+			Color textColor;
+			Color.TryParseHexString("#95E0FFFF", out textColor);
+			textMesh.color = textColor;
+
+		} else {
+			textMesh.font = defaultFont;
+			textMesh.color = Color.white;
+		}
+
+		//textMesh.renderer.sharedMaterial = ArialFont.material;*/
+	}
+
+	public void play(){
+		Application.LoadLevel ("SaveSpot");
+	}
+
+	public void load(){
+
+	}
+
+	public void settings(){
+
+	}
+
+	public void quit(){
+		Application.Quit ();
+	}
 }
