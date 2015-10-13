645d9a25d2c4cb9095a1ccba1fce392648ddea38
Elzahn Botha <elzahnbotha@mweb.co.za>
2015-10-13 22:17:22 +0200

    Reset on death works
    Hope it lasts

--- Unity Game/Assets/scripts/World/LevelSelect.cs
+++ Unity Game/Assets/scripts/World/LevelSelect.cs
@@ -45,6 +45,7 @@ public class LevelSelect : MonoBehaviour {
 		snow.SetActive (false);
 		desert.GetComponent<ParticleSystem>().enableEmission = false;
 		desert.SetActive (false);
+		spawnedLevel = false;
 	}
 
 	void Update () {
@@ -58,116 +59,112 @@ public class LevelSelect : MonoBehaviour {
 			
 			desert = GameObject.Find ("Desert");
 
-			clearParticles();
-		}
+			clearParticles ();
+		} else if (fireMist != null) {
 
-		if (myRenderer != null) {	//You are on a planet and now will customise the level
-			myRenderer.material = GetMaterial (currentLevel);
-			if(Application.loadedLevelName != "Tutorial"){
-				switch (currentLevel) {
+			if (myRenderer != null) {	//You are on a planet and now will customise the level
+				myRenderer.material = GetMaterial (currentLevel);
+				if (Application.loadedLevelName != "Tutorial") {
+					switch (currentLevel) {
 					case 1:
-					{
-						if(!spawnedLevel)
 						{
-							clearParticles();
-							fireMist.SetActive(true);
-							fireMist.GetComponent<ParticleSystem>().enableEmission = true;
-							planet.GetComponent<EnemySpawner>().spawnEnemies(20);
-							//Burning Tree, Green Tree, Big Tree, Bare Tree, Shrub
-							planet.GetComponent<SpawnTrees>().spawnTrees(110, 0, 60, 0, 50);
-							planet.GetComponent<SpawnHealthPacks>().spawnHealth(10);
-							this.GetComponent<Sounds>().playAmbienceSound(Sounds.PLANET_1_AMBIENCE);
-							this.GetComponent<Sounds>().pauseSound("ambience");
-							spawnedLevel = true;
+							if (!spawnedLevel) {
+								clearParticles ();
+								fireMist.SetActive (true);
+								fireMist.GetComponent<ParticleSystem> ().enableEmission = true;
+								planet.GetComponent<EnemySpawner> ().spawnEnemies (20);
+								//Burning Tree, Green Tree, Big Tree, Bare Tree, Shrub
+								planet.GetComponent<SpawnTrees> ().spawnTrees (110, 0, 60, 0, 50);
+								planet.GetComponent<SpawnHealthPacks> ().spawnHealth (10);
+								this.GetComponent<Sounds> ().playAmbienceSound (Sounds.PLANET_1_AMBIENCE);
+								this.GetComponent<Sounds> ().pauseSound ("ambience");
+								spawnedLevel = true;
+							}
+							break;
 						}
-						break;
-					}
 					case 2:
-					{
-						if(!spawnedLevel)
 						{
-							clearParticles();
-							rain.SetActive(true);
-							rain.GetComponent<ParticleSystem>().enableEmission = true;
-							planet.GetComponent<EnemySpawner>().spawnEnemies(35);
-							//Burning Tree, Green Tree, Big Tree, Bare Tree, Shrub
-							planet.GetComponent<SpawnTrees>().spawnTrees(0, 95, 0, 0, 40);
-							planet.GetComponent<SpawnHealthPacks>().spawnHealth(12);
-							this.GetComponent<Sounds>().playAmbienceSound(Sounds.PLANET_2_AMBIENCE);
-							this.GetComponent<Sounds>().pauseSound("ambience");
-							spawnedLevel = true;
+							if (!spawnedLevel) {
+								clearParticles ();
+								rain.SetActive (true);
+								rain.GetComponent<ParticleSystem> ().enableEmission = true;
+								planet.GetComponent<EnemySpawner> ().spawnEnemies (35);
+								//Burning Tree, Green Tree, Big Tree, Bare Tree, Shrub
+								planet.GetComponent<SpawnTrees> ().spawnTrees (0, 95, 0, 0, 40);
+								planet.GetComponent<SpawnHealthPacks> ().spawnHealth (12);
+								this.GetComponent<Sounds> ().playAmbienceSound (Sounds.PLANET_2_AMBIENCE);
+								this.GetComponent<Sounds> ().pauseSound ("ambience");
+								spawnedLevel = true;
+							}
+							break;
 						}
-						break;
-					}
 					case 3:
-					{
-						if(!spawnedLevel)
 						{
-							clearParticles();
-							snow.SetActive(true);
-							snow.GetComponent<ParticleSystem>().enableEmission = true;
-							planet.GetComponent<EnemySpawner>().spawnEnemies(10);
-							//Burning Tree, Green Tree, Big Tree, Bare Tree, Shrub
-							planet.GetComponent<SpawnTrees>().spawnTrees(0, 10, 120, 0, 50);
-							planet.GetComponent<SpawnHealthPacks>().spawnHealth(2);
-							this.GetComponent<Sounds>().playAmbienceSound(Sounds.PLANET_3_AMBIENCE);
-							this.GetComponent<Sounds>().pauseSound("ambience");
-							spawnedLevel = true;
+							if (!spawnedLevel) {
+								clearParticles ();
+								snow.SetActive (true);
+								snow.GetComponent<ParticleSystem> ().enableEmission = true;
+								planet.GetComponent<EnemySpawner> ().spawnEnemies (10);
+								//Burning Tree, Green Tree, Big Tree, Bare Tree, Shrub
+								planet.GetComponent<SpawnTrees> ().spawnTrees (0, 10, 120, 0, 50);
+								planet.GetComponent<SpawnHealthPacks> ().spawnHealth (2);
+								this.GetComponent<Sounds> ().playAmbienceSound (Sounds.PLANET_3_AMBIENCE);
+								this.GetComponent<Sounds> ().pauseSound ("ambience");
+								spawnedLevel = true;
+							}
+							break;
 						}
-						break;
-					}
 					case 4:
-					{
-						if(!spawnedLevel)
 						{
-							clearParticles();
-							desert.SetActive(true);
-							desert.GetComponent<ParticleSystem>().enableEmission = true;
-							planet.GetComponent<EnemySpawner>().spawnEnemies(40);
-							//Burning Tree, Green Tree, Big Tree, Bare Tree, Shrub
-							planet.GetComponent<SpawnTrees>().spawnTrees(0, 0, 0, 75, 35);
-							planet.GetComponent<SpawnHealthPacks>().spawnHealth(15);
-							this.GetComponent<Sounds>().playAmbienceSound(Sounds.PLANET_4_AMBIENCE);
-							this.GetComponent<Sounds>().pauseSound("ambience");
-							spawnedLevel = true;
+							if (!spawnedLevel) {
+								clearParticles ();
+								desert.SetActive (true);
+								desert.GetComponent<ParticleSystem> ().enableEmission = true;
+								planet.GetComponent<EnemySpawner> ().spawnEnemies (40);
+								//Burning Tree, Green Tree, Big Tree, Bare Tree, Shrub
+								planet.GetComponent<SpawnTrees> ().spawnTrees (0, 0, 0, 75, 35);
+								planet.GetComponent<SpawnHealthPacks> ().spawnHealth (15);
+								this.GetComponent<Sounds> ().playAmbienceSound (Sounds.PLANET_4_AMBIENCE);
+								this.GetComponent<Sounds> ().pauseSound ("ambience");
+								spawnedLevel = true;
+							}
+							break;
 						}
-						break;
-					}
 					case 5:
-					{
-						if(!spawnedLevel)
 						{
-							clearParticles();
-							desert.SetActive(true);
-							desert.GetComponent<ParticleSystem>().enableEmission = true;
-							rain.SetActive(true);
-							rain.GetComponent<ParticleSystem>().enableEmission = true;
-							planet.GetComponent<EnemySpawner>().spawnEnemies(50);
-							//Burning Tree, Green Tree, Big Tree, Bare Tree, Shrub
-							planet.GetComponent<SpawnTrees>().spawnTrees(8, 8, 8, 8, 40);
-							planet.GetComponent<SpawnHealthPacks>().spawnHealth(20);
-							this.GetComponent<Sounds>().playAmbienceSound(Sounds.PLANET_5_AMBIENCE);
-							this.GetComponent<Sounds>().pauseSound("ambience");
-							spawnedLevel = true;
+							if (!spawnedLevel) {
+								clearParticles ();
+								desert.SetActive (true);
+								desert.GetComponent<ParticleSystem> ().enableEmission = true;
+								rain.SetActive (true);
+								rain.GetComponent<ParticleSystem> ().enableEmission = true;
+								planet.GetComponent<EnemySpawner> ().spawnEnemies (50);
+								//Burning Tree, Green Tree, Big Tree, Bare Tree, Shrub
+								planet.GetComponent<SpawnTrees> ().spawnTrees (8, 8, 8, 8, 40);
+								planet.GetComponent<SpawnHealthPacks> ().spawnHealth (20);
+								this.GetComponent<Sounds> ().playAmbienceSound (Sounds.PLANET_5_AMBIENCE);
+								this.GetComponent<Sounds> ().pauseSound ("ambience");
+								spawnedLevel = true;
+							}
+							break;
 						}
-						break;
-					}
 					default:
-					{
-						GameObject.Find("Player").GetComponent<Rigidbody>().useGravity = true;
-						GameObject.Find("Player").GetComponent<FauxGravityBody>().attractor = null;
-						GameObject.Find("Player").GetComponent<Sounds>().playWorldSound(Sounds.FINISHED_GAME);
-						Application.LoadLevel("EndOfGame");
-						break;
+						{
+							GameObject.Find ("Player").GetComponent<Rigidbody> ().useGravity = true;
+							GameObject.Find ("Player").GetComponent<FauxGravityBody> ().attractor = null;
+							GameObject.Find ("Player").GetComponent<Sounds> ().playWorldSound (Sounds.FINISHED_GAME);
+							Application.LoadLevel ("EndOfGame");
+							break;
+						}
 					}
 				}
-			}
-		} else {
-			planet = GameObject.Find ("Planet");
-			if (planet != null) {	//You are on a planet so get the rendered of the planet
-				myRenderer = planet.GetComponent<Renderer> ();
-			} else {	//You are in the space ship
-				myRenderer = null;
+			} else {
+				planet = GameObject.Find ("Planet");
+				if (planet != null) {	//You are on a planet so get the rendered of the planet
+					myRenderer = planet.GetComponent<Renderer> ();
+				} else {	//You are in the space ship
+					myRenderer = null;
+				}
 			}
 		}
 	}
