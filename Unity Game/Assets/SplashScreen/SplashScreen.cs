a1379c10d98b67cb3d59024c35a0af95e842fbc2
TaniaRall <taniarall.rall@gmail.com>
2015-11-01 16:46:37 +0200

    Splash Screen ... But menu light fudged

---
+++ Unity Game/Assets/SplashScreen/SplashScreen.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * This class displays the splash screen for 'timer' amount of time and 
+ * then loads the level in 'levelToLoad'. 
+ * */
+public class SplashScreen : MonoBehaviour {
+	public float timer = 2f;
+	public string levelToLoad = "Main_Menu";
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("DisplayScene");
+	}
+
+	IEnumerator DisplayScene(){
+		yield return new WaitForSeconds (timer);
+		Application.LoadLevel (levelToLoad);
+	}
+
+}
